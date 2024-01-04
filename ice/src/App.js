a8451cd5f3fe9c4353676a0da52041a6d2dbcf1c
임성경 <5622lsk@naver.com>
2024-01-05 03:38:25 +0900

    라우터 추가

--- ice/src/App.js
+++ ice/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/mainpage';
 import ProfileSetup from './pages/profilesetup'; // ProfileSetup 컴포넌트 임포트
 import Profiledetails from './pages/profileDetails'
+import Room from "./pages/Room";
+
 const App = () => {
   return (
     <Router>
@@ -10,6 +12,7 @@ const App = () => {
         <Route path="/" element={<MainPage />} />
         <Route path="/profile-setup" element={<ProfileSetup />} /> // ProfileSetup 컴포넌트에 대한 라우트 추가
         <Route path="/profile-details" element={<Profiledetails />} /> 
+        <Route path="/room" element={<Room />} />
       </Routes>
     </Router>
   );
